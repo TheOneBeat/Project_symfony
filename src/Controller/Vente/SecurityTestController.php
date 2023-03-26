679ea4e9f9ac9724eb0c8f0205cc9bd1d574b3f6
TheOne <96834478+thecodesec@users.noreply.github.com>
2023-03-26 16:58:29 +0200

    "suppression de user, avec l'admin, avec liste des users fait"

--- src/Controller/Vente/SecurityTestController.php
+++ src/Controller/Vente/SecurityTestController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Vente;
 
+use App\Entity\Produit;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/vente', name: 'vente')]
@@ -16,33 +18,104 @@ class SecurityTestController extends AbstractController
                              UserPasswordHasherInterface $passwordHasher):Response
     {
 
+        //===================ROLE CLIENT SIMON =========================
+
         $user1 = new User();
         $user1
-            ->setLogin('mistress')
-            ->setName("Rita")
+            ->setLogin('simon')
+            ->setName("ingle")
             ->setRoles(['ROLE_CLIENT'])
-            ->setFirstName("Zouri")
-            ->setBirthday(new \DateTime());
-        $passwordUser1 = $passwordHasher->hashPassword($user1,'atir');
+            ->setFirstName("Arthur")
+            ->setBirthday(new \DateTime("1980-02-23"));
+        $passwordUser1 = $passwordHasher->hashPassword($user1,'nomis');
         $user1->setPassword($passwordUser1);
         $em->persist($user1);
 
 
-        //============================================
+        //===================ROLE CLIENT RITA =========================
 
         $user2 = new User();
         $user2
-            ->setLogin('theOne')
-            ->setName("Bill")
-            ->setRoles(['ROLE_ADMIN'])
-            ->setFirstName("Gate")
-            ->setBirthday(new \DateTime("2000-11-27"));
-        $passwordUser2 = $passwordHasher->hashPassword($user2,'billyboy');
+            ->setLogin('rita')
+            ->setName("zouri")
+            ->setRoles(['ROLE_CLIENT'])
+            ->setFirstName("Rita")
+            ->setBirthday(new \DateTime("1983-06-15"));
+        $passwordUser2 = $passwordHasher->hashPassword($user2,'atir');
         $user2->setPassword($passwordUser2);
         $em->persist($user2);
+
+        //===================ROLE ADMIN GILLES =========================
+
+        $user3 = new User();
+        $user3
+            ->setLogin('gilles')
+            ->setName("Subrenat")
+            ->setRoles(['ROLE_ADMIN'])
+            ->setFirstName("Gilou")
+            ->setBirthday(new \DateTime("1983-06-15"));
+        $passwordUser3 = $passwordHasher->hashPassword($user3,'sellig');
+        $user3->setPassword($passwordUser3);
+        $em->persist($user3);
+
+        //===================ROLE SUPER_ADMIN  =========================
+
+        $user4 = new User();
+        $user4
+            ->setLogin('sadmin')
+            ->setName("super")
+            ->setRoles(['ROLE_SUPER_ADMIN'])
+            ->setFirstName("franky")
+            ->setBirthday(new \DateTime("1983-06-15"));
+        $passwordUser4 = $passwordHasher->hashPassword($user4,'nimdas');
+        $user4->setPassword($passwordUser4);
+        $em->persist($user4);
+
+        $user5 = new User();
+        $user5
+            ->setLogin('dark')
+            ->setName("luke")
+            ->setRoles(['ROLE_CLIENT'])
+            ->setFirstName("cage")
+            ->setBirthday(new \DateTime("1983-06-15"));
+        $passwordUser5 = $passwordHasher->hashPassword($user5,'vador');
+        $user5->setPassword($passwordUser5);
+        $em->persist($user5);
 
         $em->flush();
 
         return $this->render("<body><h1>Very Good</h1></body>");
+    }
+
+
+
+    #[Route('/suppUser/{id}',
+        name: '_suppUser',
+        requirements: ['id' => '[1-9]\d*'],
+    )]
+    public function suppUser(int $id, EntityManagerInterface $em):Response
+    {
+        $userRepository = $em->getRepository(User::class);
+        $user = $userRepository->find($id);
+
+        if (is_null($user))
+            throw new NotFoundHttpException('erreur suppression produit ' . $id);
+
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('info', "suppression de l'utilisateur " . $id . ' réussie');
+
+        return $this->redirectToRoute('vente_listUser');
+    }
+
+    #[Route('/listUser', name: '_listUser')]
+    public function listUsersAction(EntityManagerInterface $em): Response
+    {
+        $userRepository = $em->getRepository(User::class);
+        $users = $userRepository->findAll();
+        $args = array(
+            'users' => $users,
+        );
+        return $this->render('user/list.html.twig', $args);
     }
 }
